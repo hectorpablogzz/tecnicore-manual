3086e06be37419bba5c383b29c9579a5bb5eba3b
G00Da <A01276941@tec.mx>
2025-04-30 14:36:01 -0600

    Prueba

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
+//hola
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Whirlpool TecniCORE - Manual',
